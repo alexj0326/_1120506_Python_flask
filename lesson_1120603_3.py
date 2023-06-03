218d24c60f55cff52271ba1f558030963f6e974e
alexj0326 <alexj0326@gmail.com>
2023-06-03 14:38:18 +0800

    MO1

--- lesson_1120603_3.py
+++ lesson_1120603_3.py
@@ -9,11 +9,30 @@ if st.button("say Hello!",key='myButton', on_click=button_click):
 else:
     st.write("Goodbye")
 
+st.divider()
 
-
-    agree = st.checkbox("我同意")
-
-    if agree : 
+agree = st.checkbox("我同意")
+if agree : 
         st.write('Great')
 
+st.divider()
 
+genre = st.radio(
+    "您喜歡的節目是什麼",
+    ('卡通', '戲劇', '愛情'))
+
+if genre == '卡通':
+    st.write('您選擇:卡通')
+elif genre == "戲劇" :
+    st.write("您選擇:戲劇")
+elif genre == '愛情':
+    st.write("您選擇:愛情")
+
+st.divider()
+
+
+option = st.selectbox(
+    '您想要如何聯絡?',
+    ('請選擇...','Email', '電話', '手機'))
+
+st.write('You selected:', option)
