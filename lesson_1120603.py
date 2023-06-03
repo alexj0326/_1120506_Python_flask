fd5c17b6acfca6fa6e2b69ae554f52979990beb1
alexj0326 <alexj0326@gmail.com>
2023-06-03 12:58:40 +0800

    MODIFY

---
+++ lesson_1120603.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+# hjhjhj
+
+#註解
+
+'''
+ghhgjkguikh
+st.write("# Hello Python")
+st.write("## Hello Alex Wang Run")
+
+'''
+
+
+st.write("# Hello Python")
+st.write("## Hello Alex Wang Run")
+
+"# Hello Java"
+"## Hello Java Script"
+
+st.divider()
+
+
+st.title("這是app的title")
+st.divider()
+
+
+st.header("這是header")
+st.divider()
+
+st.subheader("這是subheader")
+st.divider()
+
+st.text("這是text")
+st.divider()
+
+st.markdown("Hello! **world**!")
+st.divider()
+
+st.caption("這是caption")
+st.divider()
+
+st.code("a=123")
+
