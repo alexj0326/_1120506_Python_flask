82778c9703fe3334a91407c811c22b1f9f83fb25
alexj0326 <49082465+alexj0326@users.noreply.github.com>
2023-06-10 07:23:58 +0000

    MMQ

--- lesson_1120610.py
+++ lesson_1120610.py
@@ -9,4 +9,11 @@ codeSeries = codeFrame['code'].astype(str) + codeFrame['name']
 with st.sidebar:
     #st.write('請選擇股票代號:')
     #st.multiselect('請選擇股票代號:', codeFrame)
-    st.multiselect("請選擇股票:",codeSeries, max_selections=4)
+    select_Codes  = st.multiselect("請選擇股票:",codeSeries, max_selections=4)
+
+for code in select_Codes : 
+    code = code[:4] + '.TW'
+    st.write(code)
+
+
+
